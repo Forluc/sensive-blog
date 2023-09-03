d7f2efd9078e151320b8d62e7375360072e8156a
Dmitrii Masliaev <m6ytcbi@gmail.com>
2023-09-03 13:00:17 +0300

    update serialize_post and optimisations post_detail

--- blog/views.py
+++ blog/views.py
@@ -11,6 +11,7 @@ def serialize_tag(tag):
 
 
 def serialize_post(post):
+    tags = post.tags.popular()
     return {
         'title': post.title,
         'teaser_text': post.text[:200],
@@ -19,8 +20,8 @@ def serialize_post(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.annotate(Count('posts'))],
-        'first_tag_title': post.tags.first().title,
+        'tags': [serialize_tag(tag) for tag in tags],
+        'first_tag_title': tags[0].title,
     }
 
 
@@ -43,7 +44,7 @@ def index(request):
 
 
 def post_detail(request, slug):
-    post = Post.objects.select_related('author').get(slug=slug)
+    post = Post.objects.prefetch_related('author').get(slug=slug)
     comments = Comment.objects.filter(post=post).prefetch_related('author')
     serialized_comments = []
     for comment in comments:
@@ -53,16 +54,14 @@ def post_detail(request, slug):
             'author': comment.author.username,
         })
 
-    likes = post.likes.all()
-
-    related_tags = post.tags.annotate(Count('posts'))
+    related_tags = post.tags.popular()
 
     serialized_post = {
         'title': post.title,
         'text': post.text,
         'author': post.author.username,
         'comments': serialized_comments,
-        'likes_amount': len(likes),
+        'likes_amount': post.likes.count(),
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
