9e0453a0c762ad70e1a85482803312da73f50db7
Dmitrii Masliaev <m6ytcbi@gmail.com>
2023-09-07 13:52:22 +0300

    replace manager in Post

--- blog/models.py
+++ blog/models.py
@@ -32,8 +32,6 @@ class Post(models.Model):
     slug = models.SlugField('Название в виде url', max_length=200)
     image = models.ImageField('Картинка')
     published_at = models.DateTimeField('Дата и время публикации')
-    objects = PostQuerySet.as_manager()
-
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
@@ -49,6 +47,8 @@ class Post(models.Model):
         related_name='posts',
         verbose_name='Теги')
 
+    objects = PostQuerySet.as_manager()
+    
     def __str__(self):
         return self.title
 
